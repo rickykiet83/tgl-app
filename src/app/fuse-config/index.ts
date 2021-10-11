17a71c1e3fd871f75aa59d4f13b45138626696da
Kiet Pham <kietpham.dev@gmail.com>
2021-10-11 17:27:38 +1100

    config layout style

--- src/app/fuse-config/index.ts
+++ src/app/fuse-config/index.ts
@@ -8,35 +8,37 @@ import { FuseConfig } from '@fuse/types';
  * constructor method to learn more about changing these options per component basis.
  */
 
+
 export const fuseConfig: FuseConfig = {
     // Color themes can be defined in src/app/app.theme.scss
-    colorTheme      : 'theme-default',
+    colorTheme: 'theme-default',
     customScrollbars: true,
-    layout          : {
-        style    : 'vertical-layout-1',
-        width    : 'fullwidth',
-        navbar   : {
-            primaryBackground  : 'fuse-navy-700',
+    layout: {
+        style: 'horizontal-layout-1',
+        width: 'boxed',
+
+        navbar: {
+            primaryBackground: 'fuse-navy-700',
             secondaryBackground: 'fuse-navy-900',
-            folded             : false,
-            hidden             : false,
-            position           : 'left',
-            variant            : 'vertical-style-1'
+            folded: false,
+            hidden: false,
+            position: 'top',
+            variant: 'vertical-style-1'
         },
-        toolbar  : {
+        toolbar: {
             customBackgroundColor: false,
-            background           : 'fuse-white-500',
-            hidden               : false,
-            position             : 'below-static'
+            background: 'fuse-white-500',
+            hidden: false,
+            position: 'above'
         },
-        footer   : {
+        footer: {
             customBackgroundColor: true,
-            background           : 'fuse-navy-900',
-            hidden               : false,
-            position             : 'below-fixed'
+            background: 'fuse-navy-900',
+            hidden: true,
+            position: 'above-fixed'
         },
         sidepanel: {
-            hidden  : false,
+            hidden: false,
             position: 'right'
         }
     }
