1ce3ab804f792e6eb33f0af93b100e408dfeeeb8
Kiet Pham <kietpham.dev@gmail.com>
2021-10-12 20:26:08 +1100

    styling job detail - left side

--- src/app/main/my-job/my-job.module.ts
+++ src/app/main/my-job/my-job.module.ts
@@ -1,8 +1,13 @@
 import { RouterModule, Routes } from '@angular/router';
 
+import { ArrivalDateFilterComponent } from './components/filters/arrival-date-filter/arrival-date-filter.component';
 import { CompanyListComponent } from './components/company-list/company-list.component';
+import { DepartureDateFilterComponent } from './components/filters/departure-date-filter/departure-date-filter.component';
+import { FiltersComponent } from './components/filters/filters.component';
 import { FuseSharedModule } from '@fuse/shared.module';
+import { JobItemComponent } from './components/job-item/job-item.component';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -13,16 +18,13 @@ import { MatRippleModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { ModeFilterComponent } from './components/filters/mode-filter/mode-filter.component';
 import { MyJobComponent } from './my-job.component';
 import { NgModule } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
-import { FiltersComponent } from './components/filters/filters.component';
-import { ModeFilterComponent } from './components/filters/mode-filter/mode-filter.component';
-import { StatusFilterComponent } from './components/filters/status-filter/status-filter.component';
-import { DepartureDateFilterComponent } from './components/filters/departure-date-filter/departure-date-filter.component';
-import { ArrivalDateFilterComponent } from './components/filters/arrival-date-filter/arrival-date-filter.component';
 import { PortFilterComponent } from './components/filters/port-filter/port-filter.component';
 import { SortbyFilterComponent } from './components/filters/sortby-filter/sortby-filter.component';
+import { StatusFilterComponent } from './components/filters/status-filter/status-filter.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 const routes: Routes = [
     {
@@ -32,7 +34,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    declarations: [MyJobComponent, CompanyListComponent, FiltersComponent, ModeFilterComponent, StatusFilterComponent, DepartureDateFilterComponent, ArrivalDateFilterComponent, PortFilterComponent, SortbyFilterComponent],
+    declarations: [MyJobComponent, CompanyListComponent, FiltersComponent, ModeFilterComponent, StatusFilterComponent, DepartureDateFilterComponent, ArrivalDateFilterComponent, PortFilterComponent, SortbyFilterComponent, JobItemComponent],
     imports: [
         RouterModule.forChild(routes),
 
@@ -47,6 +49,7 @@ const routes: Routes = [
         MatTableModule,
         MatToolbarModule,
         MatSelectModule,
+        MatCardModule,
 
         TranslateModule,
 
